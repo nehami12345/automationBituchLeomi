e969eda236f9d5e33a0fd5a91ec0b91734cf0ebb
heineman <b0527676318@gmail.com>
2026-02-12 03:19:35 +0200

    complete

---
+++ src/test/java/tests/TestListener.java
@@ -0,0 +1,29 @@
+package tests;
+
+import io.qameta.allure.Attachment;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class TestListener extends TestWatcher {
+
+    private WebDriver driver;
+
+    public TestListener(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    protected void failed(Throwable e, Description description) {
+        System.out.println("הטסט נכשל! מצלם מסך...");
+        captureScreenshot(driver);
+    }
+
+    @Attachment(value = "Page Screenshot", type = "image/png")
+    public byte[] captureScreenshot(WebDriver driver) {
+        // זו הפעולה שמצלמת ומצרפת לדוח
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
--- src/test/java/tests/navigateTest.java
+++ src/test/java/tests/navigateTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.example.HomePage;
 import org.example.navigateFromKitbaut;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -11,9 +10,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -25,21 +22,19 @@ public class navigateTest{
     private String linkToClick;
     private String expectedResult;
 
-    // 3. בנאי (Constructor) - מקבל את הפרמטרים מהרשימה למטה
     public navigateTest(String linkToClick, String expectedResult) {
         this.linkToClick = linkToClick;
         this.expectedResult = expectedResult;
     }
 
-    // 4. הגדרת הנתונים - כל שורה היא ריצה נפרדת של הטסט
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                // { "הטקסט עליו לוחצים", "הטקסט המצופה בכותרת/פירורי לחם" }
                 { "ילד נכה", "ילד נכה" },
                 { "אזרח ותיק", "אזרח ותיק" },
                 { "נפגעי עבודה", "נפגעי עבודה" },
-                { "מזונות", "מזונות" }
+                { "הבטחת הכנסה", "הבטחת הכנסה" },
+                {"מענק לימודים", "מענק לימודים"}
         });
     }
     @BeforeClass
@@ -51,24 +46,26 @@ public class navigateTest{
 
     @Test
     public void testNavigateAndVerifyBreadcrumbs() {
-        // 1. ניווט ידני לדף הראשי כדי להבטיח שאנחנו במקום הנכון לפני יצירת האובייקט
-        driver.get("https://www.btl.gov.il/benefits/Pages/default.aspx");
+        try {
+            driver.get("https://www.btl.gov.il/benefits/Pages/default.aspx");
 
-        // 2. כעת בטוח ליצור את האובייקט (כי אנחנו בדף הנכון)
-        navigateFromKitbaut page = new navigateFromKitbaut(driver);
+            navigateFromKitbaut page = new navigateFromKitbaut(driver);
 
-        System.out.println("בודק כעת את הקטגוריה: " + this.linkToClick);
+            System.out.println("בודק כעת את הקטגוריה: " + this.linkToClick);
 
-        // 3. לחיצה על הקטגוריה
-        page.clickOnSubCategory(this.linkToClick);
+            page.clickOnSubCategory(this.linkToClick);
 
-        // 4. בדיקה
-        String actualBreadcrumb = page.getBreadcrumbsText();
+            String actualBreadcrumb = page.getBreadcrumbsText();
 
-        Assert.assertTrue("שגיאה! הטקסט בדף לא תואם.\n" +
-                        "ציפינו לראות: " + this.expectedResult + "\n" +
-                        "קיבלנו בפועל: " + actualBreadcrumb,
-                actualBreadcrumb.contains(this.expectedResult));
+            Assert.assertTrue("שגיאה! הטקסט בדף לא תואם.\n" +
+                            "ציפינו לראות: " + this.expectedResult + "\n" +
+                            "קיבלנו בפועל: " + actualBreadcrumb,
+                    actualBreadcrumb.contains(this.expectedResult));
+        }
+        catch (AssertionError | Exception e) {
+            new TestListener(driver).captureScreenshot(driver);
+            throw e; // זרוק את השגיאה כדי שהטסט יסומן כנכשל
+        }
     }
 
     @AfterClass
